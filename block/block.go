3f956c3bd37341e1bb70dec4a2ab92444c112b91
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-06-14 13:27:06 +0100

    Refactor TestNewBlock

--- block/block.go
+++ block/block.go
@@ -8,6 +8,11 @@ type Block struct {
 	data []byte
 }
 
+// TODO add timestamp
+// TODO add height
+// TODO add hash
+// TODO add prevHash
+// TODO add owner
 func NewBlock(data []byte) *Block {
 	dataHex := make([]byte, hex.EncodedLen(len(data)))
 	hex.Encode(dataHex, data)
--- block/block_test.go
+++ block/block_test.go
@@ -6,33 +6,29 @@ import (
 )
 
 func TestNewBlock(t *testing.T) {
-	t.Log("Block")
+	t.Log("TestNewBlock")
 	{
-		t.Log("TestNewBlock")
+		data := []byte("\"This is JSON string\"")
+		t.Logf("\tGiven string data (%s)", data)
 		{
-			data := []byte("\"This is JSON string\"")
-			t.Logf("\tGiven string data (%s)", data)
-			{
-				block := NewBlock(data)
-				if block == nil {
-					t.Errorf("\t\tShould return new Block, got: nil")
-				}
-				t.Log("\t\tShould return new Block")
+			block := NewBlock(data)
+			if block == nil {
+				t.Fatalf("\t\tShould return new Block, got: nil")
+			}
+			t.Log("\t\tShould return new Block")
 
-				expected := make([]byte, hex.EncodedLen(len(data)))
-				hex.Encode(expected, data)
-				actual := block.data
-				if len(actual) != len(expected) {
-					t.Errorf("\t\tShould save data of proper length (%v), got: %v", len(expected), len(actual))
-				}
-				t.Logf("\t\tShould save data of proper length (%v)", len(expected))
+			expected := make([]byte, hex.EncodedLen(len(data)))
+			hex.Encode(expected, data)
+			actual := block.data
+			if len(actual) != len(expected) {
+				t.Fatalf("\t\tShould save data of proper length (%v), got: %v", len(expected), len(actual))
+			}
+			t.Logf("\t\tShould save data of proper length (%v)", len(expected))
 
-				for i, b := range actual {
-					if b != expected[i] {
-						t.Errorf("\t\tShould contain the same byte at index: %v, got: %v ", i, b)
-					}
+			for i, b := range actual {
+				if b != expected[i] {
+					t.Fatalf("\t\tShould contain the same byte at index: %v, got: %v ", i, b)
 				}
-				t.Log("\t\tShould save data as hex")
 			}
 			t.Log("\t\tShould save data as hex")
 		}
