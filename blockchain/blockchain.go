16ddc6306e9d2fb5f9e4804b4859635680f7b82e
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-06-16 13:31:15 +0100

    Add block package to blockchain; fail New fn test

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -2,11 +2,18 @@
 // It depends on block package
 package blockchain
 
+import (
+	"github.com/starchain/block"
+)
+
 // Blockchain struct consists of the slice of Blocks (the chain).
 // It allows basic operations such as adding block, checking height,
 // checking blockchain integrity, fetching owner's blocks,
 // getting block by id
-type Blockchain struct{}
+type Blockchain struct {
+	chain []block.Block
+	// TODO mutex
+}
 
 // Factory function returning new Blockchain
 func New() *Blockchain {
--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -13,7 +13,10 @@ func TestNew(t *testing.T) {
 			if blockchain == nil {
 				t.Fatalf("\t\tShould return new Blockchain, got:\nnil")
 			}
-			t.Log("\t\tShould return new Blockchain")
+			if len(blockchain.chain) <= 0 {
+				t.Fatalf("\t\tShould contain genesis block, but is empty")
+			}
+			t.Log("\t\tShould return new Blockchain with Genesis Block inside")
 		}
 	}
 }
