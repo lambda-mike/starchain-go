ca6e36a9724400c06b1dafa09f468289ba1169dc
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-06-26 19:20:41 +0100

    Add draft of failing test for RequestValidation

--- api/api_test.go
+++ api/api_test.go
@@ -7,10 +7,13 @@ import (
 	"testing"
 )
 
+func init() {
+	RegisterHandlers()
+}
+
 func TestHello(t *testing.T) {
 	t.Log("TestHello")
 	{
-		RegisterHandlers()
 		t.Log("\tWhen called at /hello")
 		{
 			req, err := http.NewRequest("GET", "/hello", nil)
@@ -33,6 +36,36 @@ func TestHello(t *testing.T) {
 				t.Fatalf("\t\tShould return string \"hello\", got: \"%s\"", body)
 			}
 			t.Log("\t\tShould return string \"hello\"")
+		}
+	}
+}
+
+func TestRequestValidation(t *testing.T) {
+	t.Log("TestRequestValidation")
+	{
+		t.Log("\tWhen called at /requestValidation")
+		{
+			// TODO create body from ValidationRequest
+			req, err := http.NewRequest("POST", "/ValidationRequest", nil)
+			if err != nil {
+				t.Fatalf("\t\tShould be able to submit a validation request, got err: %v", err)
+			}
+			t.Log("\t\tShould be able to submit a validation request")
+			recorder := httptest.NewRecorder()
+			http.DefaultServeMux.ServeHTTP(recorder, req)
+			if recorder.Code != 200 {
+				t.Fatalf("\t\tShould get response 200 OK, got: %v", recorder.Code)
+			}
+			t.Log("\t\tShould get response 200 OK")
+			body, err := ioutil.ReadAll(recorder.Body)
+			if err != nil {
+				t.Fatalf("\t\tShould not return err, got: %v", err)
+			}
+			t.Log("\t\tShould not return err")
+			if string(body) != "TODO" {
+				t.Fatalf("\t\tShould return correct message, got: \"%s\"", body)
+			}
+			t.Log("\t\tShould return correct message")
 		}
 	}
 }
