14b86432e399b13aa43853fda247a2ed9375cd6a
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-06-27 12:31:31 +0100

    Add Address struct, error handling in requestValidation

--- api/api.go
+++ api/api.go
@@ -3,11 +3,16 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/starchain/contracts"
 	"log"
 	"net/http"
 )
+
+type Address = struct {
+	Address string
+}
 
 var blockchain *contracts.Blockchain
 
@@ -19,11 +24,30 @@ func RegisterHandlers(blockchain *contracts.Blockchain) {
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-	log.Println("hello")
+	log.Println("INFO: hello")
 	fmt.Fprint(res, "hello")
 }
 
 func requestValidation(res http.ResponseWriter, req *http.Request) {
-	log.Println("requestValidation")
+	log.Println("INFO: requestValidation")
+	var addr Address
+	if req.Body == nil {
+		log.Println("ERR: requestValidation: request body is nil")
+		res.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(res, "Error occurred when decoding address from JSON: empty body")
+		return
+	}
+	if err := json.NewDecoder(req.Body).Decode(&addr); err != nil {
+		log.Println("ERR: requestValidation: ", err)
+		res.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(res, "Error occurred when decoding address from JSON")
+		return
+	}
+	if addr.Address == "" {
+		log.Println("ERR: requestValidation: empty address field")
+		res.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(res, "address is required")
+		return
+	}
 	fmt.Fprint(res, "TODO ...")
 }
