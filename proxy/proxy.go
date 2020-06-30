50fdf3eb52d4d7a65158fda4339c9e6c21e27055
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-06-30 18:11:57 +0100

    Add draft of BlockchainProxy

---
+++ proxy/proxy.go
@@ -0,0 +1,52 @@
+// proxy package provides BlockchainProxy struct which translates
+// blockchain specific types to contracts and vice-versa.
+package proxy
+
+import (
+	"errors"
+	"github.com/starchain/blockchain"
+	"github.com/starchain/contracts"
+)
+
+type BlockchainProxy struct {
+	blockchain *blockchain.Blockchain
+}
+
+func New(blockchain *blockchain.Blockchain) BlockchainProxy {
+	return BlockchainProxy{blockchain}
+}
+
+func (b BlockchainProxy) RequestMessageOwnershipVerification(addr string) (string, error) {
+	// TODO
+	return addr + " OK", nil
+}
+
+func (b BlockchainProxy) GetBlockByHeight(h int) (contracts.Block, error) {
+	// TODO
+	var block contracts.Block
+	switch h {
+	default:
+		return block, errors.New("TODO")
+	}
+}
+
+func (b BlockchainProxy) GetBlockByHash(h string) (contracts.Block, error) {
+	// TODO
+	var block contracts.Block
+	switch h {
+	default:
+		return block, errors.New("TODO")
+	}
+}
+
+func (b BlockchainProxy) GetStarsByWalletAddress(addr string) []string {
+	// TODO
+	var stars []string = make([]string, 0)
+	return stars
+}
+
+func (b BlockchainProxy) SubmitStar(star contracts.StarData) (contracts.Block, error) {
+	// TODO
+	var block contracts.Block
+	return block, nil
+}
