fae6af0f66556d2330af5d206d430c5957422922
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-07-01 17:20:39 +0100

    Finish proxy implementation and refactoring

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -7,6 +7,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"github.com/starchain/block"
 	"github.com/starchain/blockchain"
 	"github.com/starchain/contracts"
 )
@@ -19,26 +20,20 @@ func New(blockchain *blockchain.Blockchain) BlockchainProxy {
 	return BlockchainProxy{blockchain}
 }
 
-func (b BlockchainProxy) RequestMessageOwnershipVerification(addr string) (string, error) {
-	return b.blockchain.RequestMessageOwnershipVerification(addr)
+func (bp BlockchainProxy) RequestMessageOwnershipVerification(addr string) (string, error) {
+	return bp.blockchain.RequestMessageOwnershipVerification(addr)
 }
 
-func (b BlockchainProxy) GetBlockByHeight(h int) (contracts.Block, error) {
+func (bp BlockchainProxy) GetBlockByHeight(h int) (contracts.Block, error) {
 	var result contracts.Block
-	block, err := b.blockchain.GetBlockByHeight(h)
+	block, err := bp.blockchain.GetBlockByHeight(h)
 	if err == nil {
-		result.Body = string(block.GetData())
-		// TODO reuse utils.HashToStr fn
-		result.Hash = fmt.Sprintf("%x", block.GetHash())
-		result.Height = block.GetHeight()
-		result.Owner = block.GetOwner()
-		result.PreviousBlockHash = fmt.Sprintf("%x", block.GetPrevHash())
-		result.Time = block.GetTimestamp()
+		result = MapBlockToContract(block)
 	}
 	return result, err
 }
 
-func (b BlockchainProxy) GetBlockByHash(h string) (contracts.Block, error) {
+func (bp BlockchainProxy) GetBlockByHash(h string) (contracts.Block, error) {
 	var (
 		result contracts.Block
 		hash   [sha256.Size]byte
@@ -53,25 +48,38 @@ func (b BlockchainProxy) GetBlockByHash(h string) (contracts.Block, error) {
 	for i, _ := range buf {
 		hash[i] = buf[i]
 	}
-	block, err := b.blockchain.GetBlockByHash(hash)
+	block, err := bp.blockchain.GetBlockByHash(hash)
 	if err == nil {
-		result.Body = string(block.GetData())
-		// TODO reuse utils.HashToStr fn
-		result.Hash = fmt.Sprintf("%x", block.GetHash())
-		result.Height = block.GetHeight()
-		result.Owner = block.GetOwner()
-		result.PreviousBlockHash = fmt.Sprintf("%x", block.GetPrevHash())
-		result.Time = block.GetTimestamp()
+		result = MapBlockToContract(block)
 	}
 	return result, err
 }
 
-func (b BlockchainProxy) GetStarsByWalletAddress(addr string) []string {
-	return b.blockchain.GetStarsByWalletAddress(addr)
+func (bp BlockchainProxy) GetStarsByWalletAddress(addr string) []string {
+	return bp.blockchain.GetStarsByWalletAddress(addr)
 }
 
-func (b BlockchainProxy) SubmitStar(star contracts.StarData) (contracts.Block, error) {
-	var block contracts.Block
-	// TODO
-	return block, errors.New("TODO")
+func (bp BlockchainProxy) SubmitStar(star contracts.StarData) (contracts.Block, error) {
+	var req blockchain.StarRequest
+	req.Addr = star.Address
+	req.Msg = star.Message
+	req.StarData = star.Data
+	req.Sig = star.Signature
+	b, err := bp.blockchain.SubmitStar(req)
+	if err != nil {
+		return contracts.Block{}, err
+	}
+	return MapBlockToContract(b), nil
+}
+
+func MapBlockToContract(block *block.Block) contracts.Block {
+	var result contracts.Block
+	result.Body = string(block.GetData())
+	// TODO reuse utils.HashToStr fn
+	result.Hash = fmt.Sprintf("%x", block.GetHash())
+	result.Height = block.GetHeight()
+	result.Owner = block.GetOwner()
+	result.PreviousBlockHash = fmt.Sprintf("%x", block.GetPrevHash())
+	result.Time = block.GetTimestamp()
+	return result
 }
